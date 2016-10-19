1091a2b8ddc14f6635b9cfc560f94231100ef64c
Matthew Horn <mwhorn1008@gmail.com>
2016-10-19 14:49:10 -0500

    Updates

--- src/hriClass/src/hri2.cpp
+++ src/hriClass/src/hri2.cpp
@@ -7,19 +7,20 @@
 #include <geometry_msgs/Point.h>
 #include <boost/thread/thread.hpp>
 #include <turtlesim/Spawn.h>
-#include <turtlesim/Pose.h> // x, y, theta
+#include <turtlesim/Pose.h>  // x, y, theta
 #include <turtlesim/TeleportAbsolute.h>
 #include <std_srvs/Empty.h>
+#include <math.h>
 
 // Creating a class for a Spinner driver
 class SpinnerDriver {
-private:
+ private:
   // Nothing outside of this class should even need to see this
   ros::NodeHandle n;
   ros::Publisher velPub;
   geometry_msgs::Twist command;
 
-public:
+ public:
   // Something others may be able to see
 
   // Creating a constructor that takes a ROS node handle and generates a
@@ -34,7 +35,7 @@ public:
     ros::Duration(0.5).sleep();
 
     while (n.ok()) {
-      command.angular.z = 0.1;
+      command.angular.z = 0.075;
       velPub.publish(command);
       ros::Duration(1.1).sleep();
     }
@@ -43,7 +44,7 @@ public:
 
 // Creating a that will attempt to follow the Spinner
 class FollowerDriver {
-private:
+ private:
   // Nothing outside of this class should even need to see this
   ros::NodeHandle n;
   ros::Publisher velPub;
@@ -53,15 +54,11 @@ private:
   geometry_msgs::Twist command;
   float theta;
 
-public:
+ public:
   // Creating a constructor that takes a ROS node handle and generates a
   // publisher
-  void poseCallback(const turtlesim::Pose &msg)
-  {
-    theta = msg.theta;
-  }
-  void pose2Callback(const turtlesim::Pose &msg)
-  {
+  void poseCallback(const turtlesim::Pose& msg) { theta = msg.theta; }
+  void pose2Callback(const turtlesim::Pose& msg) {
     currentPose.theta = msg.theta;
     currentPose.x = msg.x;
     currentPose.y = msg.y;
@@ -70,20 +67,59 @@ public:
   FollowerDriver(ros::NodeHandle& nh) {
     n = nh;
     velPub = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 1);
-    spinPose = n.subscribe("/turtle1/pose", 10, &FollowerDriver::poseCallback, this);
-    myPose = n.subscribe("/turtle2/pose", 10, &FollowerDriver::pose2Callback, this);
+    spinPose =
+        n.subscribe("/turtle1/pose", 10, &FollowerDriver::poseCallback, this);
+    myPose =
+        n.subscribe("/turtle2/pose", 10, &FollowerDriver::pose2Callback, this);
   }
 
   bool follow() {
     std::cout << "Will now try to walk past Spinner.\n";
     ros::Duration(0.5).sleep();
-    float vel;
+    float vel, angle, control, height, width, velAdj, angAdj;
+    height = 0.0;
+    width = 0.0;
+    velAdj = 1.0;
+    angAdj = 1.0;
+
+    ros::spinOnce();
 
     while (n.ok()) {
-      vel = 0.025*std::sqrt((currentPose.x-9.5)^2+(currentPose.y-5.5)^2);
-      float angle = std::atan();
+      // Implementing Forcefield around the spinner, eggshell size
+      // Begin turning away if it is too close
+      width = -4.5;
+      velAdj = 1.0;
+      angAdj = 1.0;
+      height = sin(theta) * 0.5 + 0.75;
+
+      // If passed the turtle, resume normal travel
+      if (currentPose.x - 5.0 > 0.0) {
+        // std::cout << "Passed"
+        //           << "\n";
+        width = 0.0;
+        velAdj = 1.0;
+        angAdj = 1.0;
+        height = 0.0;
+      }
+
+      vel = 0.05 * velAdj *
+            std::sqrt(std::pow(currentPose.x - (9.5 + width), 2) +
+                      std::pow(currentPose.y - (5.5 + height), 2));
+      if (vel <= 0.4 && fabs(currentPose.x - 9.5) < 0.25) {
+        std::cout << "I made it!";
+        return true;
+      } else if (vel <= 0.04)
+        vel = 0.4;
+      angle =
+          atan2((5.5 + height - currentPose.y), (9.5 + width - currentPose.x));
+      control = 0.3 * angAdj * (angle - currentPose.theta);
+      // std::cout << height << " " << theta << "\n";
+      command.linear.x = vel;
+      command.angular.z = control;
+
       velPub.publish(command);
-      ros::Duration(1.1).sleep();
+      ros::spinOnce();
+      ros::Duration(0.5).sleep();
     }
     return true;
   }
@@ -99,27 +135,28 @@ int main(int argc, char** argv) {
   SpinnerDriver Spinner(nh);
 
   ros::service::waitForService("spawn");
-  ros::ServiceClient add_turtle =
-  nh.serviceClient<turtlesim::Spawn>("spawn");
+  ros::ServiceClient add_turtle = nh.serviceClient<turtlesim::Spawn>("spawn");
   turtlesim::Spawn srv;
   add_turtle.call(srv);
   ros::ServiceClient teleport_turtle =
-  nh.serviceClient<turtlesim::TeleportAbsolute>("turtle1/teleport_absolute");
+      nh.serviceClient<turtlesim::TeleportAbsolute>(
+          "turtle1/teleport_absolute");
   turtlesim::TeleportAbsolute srv2;
   srv2.request.x = 5.0;
-  srv2.request.y = 5.5;  
-  srv2.request.theta = 0;
+  srv2.request.y = 5.5;
+  srv2.request.theta = 0.0;
   teleport_turtle.call(srv2);
   ros::ServiceClient teleport_turtle2 =
-  nh.serviceClient<turtlesim::TeleportAbsolute>("turtle2/teleport_absolute");
+      nh.serviceClient<turtlesim::TeleportAbsolute>(
+          "turtle2/teleport_absolute");
   turtlesim::TeleportAbsolute srv3;
   srv3.request.x = 0.5;
-  srv3.request.y = 5.5;  
-  srv3.request.theta = 0;
+  srv3.request.y = 5.5;
+  srv3.request.theta = 0.0;
   teleport_turtle2.call(srv3);
 
   ros::service::waitForService("clear");
-  ros::ServiceClient clear =  nh.serviceClient<std_srvs::Empty>("clear");
+  ros::ServiceClient clear = nh.serviceClient<std_srvs::Empty>("clear");
   std_srvs::Empty empt;
   clear.call(empt);
 
---
+++ src/hriClass/src/hri3.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Point.h>
+#include <boost/thread/thread.hpp>
+#include <turtlesim/Spawn.h>
+#include <turtlesim/Pose.h>  // x, y, theta
+#include <turtlesim/TeleportAbsolute.h>
+#include <std_srvs/Empty.h>
+#include <math.h>
+
+// Creating a class for a turtle2 driver
+class turtle2Driver {
+ private:
+  // Nothing outside of this class should even need to see this
+  ros::NodeHandle n;
+  ros::Publisher velPub;
+  geometry_msgs::Twist command;
+
+ public:
+  // Something others may be able to see
+
+  // Creating a constructor that takes a ROS node handle and generates a
+  // publisher
+  turtle2Driver(ros::NodeHandle& nh) {
+    n = nh;
+    velPub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
+  }
+
+  void lead() {
+    std::cout << "Will now try to lead the way for my turtle1!\n";
+    ros::Duration(0.5).sleep();
+
+    while (n.ok()) {
+      command.angular.z = 0.075;
+      velPub.publish(command);
+      ros::Duration(1.1).sleep();
+    }
+  }
+};
+
+// Creating a that will attempt to follow the turtle2
+class turtle1Driver {
+ private:
+  // Nothing outside of this class should even need to see this
+  ros::NodeHandle n;
+  ros::Publisher velPub;
+  ros::Subscriber speechDecision;
+  turtlesim::Pose currentPose;
+  geometry_msgs::Twist command;
+  float theta;
+  std_msgs::String currentCommand;
+
+ public:
+  // Creating a constructor that takes a ROS node handle and generates a
+  // publisher
+  void speechCallback(const turtlesim::Pose& msg) {
+    // SET CURRENTCOMMAND.DATA towhat the current job is
+  }
+
+  turtle1Driver(ros::NodeHandle& nh) {
+    n = nh;
+    currentCommand.data = "Null";
+    velPub = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 1);
+    speechDecision = n.subscribe("/hlpr_speech_commands", 10,
+                                 &turtle1Driver::speechCallback, this);
+  }
+
+  bool follow() {
+    std::cout << "Will now try to walk past turtle2.\n";
+    ros::Duration(0.5).sleep();
+    float vel, angle, control, height, width, velAdj, angAdj;
+    height = 0.0;
+    width = 0.0;
+    velAdj = 1.0;
+    angAdj = 1.0;
+
+    ros::spinOnce();
+
+    while (n.ok()) {
+      // Implementing Forcefield around the turtle2, eggshell size
+      // Begin turning away if it is too close
+      width = -4.5;
+      velAdj = 1.0;
+      angAdj = 1.0;
+      height = sin(theta) * 0.5 + 0.75;
+
+      // If passed the turtle, resume normal travel
+      if (currentPose.x - 5.0 > 0.0) {
+        // std::cout << "Passed"
+        //           << "\n";
+        width = 0.0;
+        velAdj = 1.0;
+        angAdj = 1.0;
+        height = 0.0;
+      }
+
+      vel = 0.05 * velAdj *
+            std::sqrt(std::pow(currentPose.x - (9.5 + width), 2) +
+                      std::pow(currentPose.y - (5.5 + height), 2));
+      if (vel <= 0.4 && fabs(currentPose.x - 9.5) < 0.25) {
+        std::cout << "I made it!";
+        return true;
+      } else if (vel <= 0.04)
+        vel = 0.4;
+      angle =
+          atan2((5.5 + height - currentPose.y), (9.5 + width - currentPose.x));
+      control = 0.3 * angAdj * (angle - currentPose.theta);
+      // std::cout << height << " " << theta << "\n";
+      command.linear.x = vel;
+      command.angular.z = control;
+
+      velPub.publish(command);
+      ros::spinOnce();
+      ros::Duration(0.5).sleep();
+    }
+    return true;
+  }
+};
+
+int main(int argc, char** argv) {
+  // init the ROS node
+  ros::init(argc, argv, "turtle2turtle1");
+  ros::NodeHandle nh;
+
+  // Creates and runs the drunken turtle
+  turtle1Driver turtle1(nh);
+  turtle2Driver turtle2(nh);
+
+  ros::service::waitForService("spawn");
+  ros::ServiceClient add_turtle = nh.serviceClient<turtlesim::Spawn>("spawn");
+  turtlesim::Spawn srv;
+  add_turtle.call(srv);
+  ros::ServiceClient teleport_turtle =
+      nh.serviceClient<turtlesim::TeleportAbsolute>(
+          "turtle1/teleport_absolute");
+  turtlesim::TeleportAbsolute srv2;
+  srv2.request.x = 5.0;
+  srv2.request.y = 5.5;
+  srv2.request.theta = 0.0;
+  teleport_turtle.call(srv2);
+  ros::ServiceClient teleport_turtle2 =
+      nh.serviceClient<turtlesim::TeleportAbsolute>(
+          "turtle2/teleport_absolute");
+  turtlesim::TeleportAbsolute srv3;
+  srv3.request.x = 0.5;
+  srv3.request.y = 5.5;
+  srv3.request.theta = 0.0;
+  teleport_turtle2.call(srv3);
+
+  ros::service::waitForService("clear");
+  ros::ServiceClient clear = nh.serviceClient<std_srvs::Empty>("clear");
+  std_srvs::Empty empt;
+  clear.call(empt);
+
+  boost::thread thread_b(&turtle2Driver::lead, turtle2);
+}
